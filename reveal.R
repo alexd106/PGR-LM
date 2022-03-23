5279f6edf015023c4865289d9b174fed0d0f4926
alexd106 <alexd106@gmail.com>
2022-03-23 13:43:02 +0000

    update exercise and solutions listings

--- reveal.R
+++ reveal.R
@@ -3,7 +3,7 @@ show_text_GDE <- TRUE    # ex GDE
 show_text_lm1 <- TRUE    # ex LM1
 show_text_lm2 <- TRUE    # ex LM2
 show_text_lm3 <- TRUE   # ex LM3
-show_text_lm4 <- FALSE    # ex LM4
+show_text_lm4 <- TRUE    # ex LM4
 
 # Change to TRUE to show lectures
 show_lecture <- TRUE
